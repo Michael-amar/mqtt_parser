a99e5213a146c8a7de30baef0750b8e7d5e9648b
Michael Amar <michaela@bugsec.com>
2024-10-14 11:15:15 +0300

    pip fix

--- mqtt_parser/__init__.py
+++ mqtt_parser/__init__.py
@@ -1,8 +1,8 @@
 from mqtt_parser.mqtt_parser import parse
 
 
-# __all__ = ['parse']
+__all__ = ['parse']
 
-# def __dir__():
-#     """Custom dir to limit exposed attributes."""
-#     return __all__
+def __dir__():
+    """Custom dir to limit exposed attributes."""
+    return __all__
